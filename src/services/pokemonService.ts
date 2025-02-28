669c34410d660fb56a1b6c912ff104d84ea5fa55
Luis Valladares <ls.valladares7@gmail.com>
2025-02-28 10:04:43 -0600

    refactor getPokemonDetails service

--- src/services/pokemonService.ts
+++ src/services/pokemonService.ts
@@ -1,5 +1,3 @@
-import { PokemonDetails } from "../types/pokemon";
-
 export const getPokemons = async (limit: number, offset: number) => {
   const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
   const response = await fetch(url);
@@ -13,33 +11,6 @@ export const getPokemonDetails = async (id: string) => {
   const response = await fetch(url);
   if (!response.ok) throw new Error('Failed to fetch pokemon data');
   const data = await response.json();
-
-  const pokemonDetails = data.stats.reduce((accum: PokemonDetails, stat: any) => {
-    switch (stat.stat.name) {
-      case "hp":
-        accum.hp = stat.base_stat;
-        break;
-      case "attack":
-        accum.attack = stat.base_stat;
-        break;
-      case "defense":
-        accum.defense = stat.base_stat;
-        break;
-      case "special-attack":
-        accum.specialAttack = stat.base_stat;
-        break;
-      case "special-defense":
-        accum.specialDefense = stat.base_stat;
-        break;
-      case "speed":
-        accum.speed = stat.base_stat;
-        break;
-    }
-    return accum;
-  }, {});
-
-  pokemonDetails.abilities = data.abilities.map((ability: any) => ability.ability.name);
-
-  return pokemonDetails;
+  return data;
 }
 
--- src/store/slices/pokemonsSlice.ts
+++ src/store/slices/pokemonsSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { Pokemon } from "../../types/pokemon";
+import { Pokemon, PokemonDetails } from "../../types/pokemon";
 
 const initialState: Pokemon[] = [];
 
@@ -9,21 +9,41 @@ const pokemonsSlice = createSlice({
   reducers: {
     addPokemons: (state, action) => {
       const pokemonsToAdd = action.payload.map((pokemon: Pokemon) => {
-        const pokemonId = pokemon.url.split("/")[6];
-        const pokemonImageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonId}.svg`;
-
-        return {
-          ...pokemon,
-          id: pokemonId,
-          image: pokemonImageUrl,
-        }
+        pokemon.id = pokemon.url.split("/")[6];
+        pokemon.image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`;
+        return pokemon;
       });
       return [...state, ...pokemonsToAdd];
     },
     addPokemonDetails: (state, action) => {
       const { id, details } = action.payload;
+      const pokemonDetails: PokemonDetails = details.stats.reduce((accum: PokemonDetails, {stat, base_stat}: any) => {
+        switch (stat.name) {
+          case "hp":
+            accum.hp = base_stat;
+            break;
+          case "attack":
+            accum.attack = base_stat;
+            break;
+          case "defense":
+            accum.defense = base_stat;
+            break;
+          case "special-attack":
+            accum.specialAttack = base_stat;
+            break;
+          case "special-defense":
+            accum.specialDefense = base_stat;
+            break;
+          case "speed":
+            accum.speed = base_stat;
+            break;
+        }
+        return accum;
+      }, {});
+      pokemonDetails.abilities = details.abilities.map((ability: any) => ability.ability.name);
+
       const pokemonIndex = state.findIndex(pokemon => pokemon.id === id);
-      state[pokemonIndex].details = details;
+      state[pokemonIndex].details = pokemonDetails;
     }
   },
 });
