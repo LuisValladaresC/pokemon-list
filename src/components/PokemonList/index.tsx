a0357db27208c40cbb0f96465a36c5539e7f930c
Luis Valladares <ls.valladares7@gmail.com>
2025-02-28 10:05:45 -0600

    Float navigation buttons

--- src/components/PokemonList/index.tsx
+++ src/components/PokemonList/index.tsx
@@ -6,15 +6,14 @@ import { addPokemons } from "../../store/slices/pokemonsSlice";
 import PokemonCard from "../PokemonCard";
 
 interface PokemonListProps {
-  page: number;
   search: string;
 }
 
-const PokemonList = ({ page, search }: PokemonListProps) => {
+const PokemonList = ({ search }: PokemonListProps) => {
   const dispatch = useDispatch();
   const pokemons: Pokemon[] = useSelector((state: any) => state.pokemons);
   const [pokemonsToShow, setPokemonsToShow] = useState<Pokemon[]>([]);
-
+  const [page, setPage] = useState(1)
   const pokemonsPerPage = 20;
 
   useEffect(() => {
@@ -31,6 +30,10 @@ const PokemonList = ({ page, search }: PokemonListProps) => {
   }, [page, pokemons]);
 
   useEffect(() => {
+    window.scroll(window.scrollX, 0)
+  }, [page]);
+
+  useEffect(() => {
     setPokemonsToShow(getPokemonsToShow());
   }, [search]);
 
@@ -39,17 +42,36 @@ const PokemonList = ({ page, search }: PokemonListProps) => {
       const pokemonsByPage = pokemons.slice((page - 1) * pokemonsPerPage, page * pokemonsPerPage);
       return pokemonsByPage;
     } else {
-      const filteredPokemons = pokemons.filter(pokemon => pokemon.name.includes(search));
+      const filteredPokemons = pokemons.filter(pokemon => pokemon.name.includes(search.toLowerCase()));
       return filteredPokemons;
     }
   }
 
   return (
-    <ul className="flex flex-wrap justify-center gap-4">
-      {pokemonsToShow.map(pokemon => (
-        <PokemonCard key={pokemon.id} pokemon={pokemon} />
-      ))}
-    </ul>
+    <>
+      <ul className="flex flex-wrap justify-center gap-4">
+        {pokemonsToShow.map(pokemon => (
+          <PokemonCard key={pokemon.id} pokemon={pokemon} />
+        ))}
+      </ul>
+
+      {!search && 
+        <div className="fixed bottom-0 flex justify-center my-6">
+          {page > 1 &&
+            <button
+              onClick={() => setPage(page - 1)}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-l cursor-pointer">
+              Previous
+            </button>
+          }
+          <button
+            onClick={() => setPage(page + 1)}
+            className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ${page > 1 ? "rounded-r" : "rounded"} cursor-pointer`}>
+            Next
+          </button>
+        </div>
+      }
+    </>
   );
 }
 
